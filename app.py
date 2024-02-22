3dd76106939b7d45d29cf855f6d0e672a8b0c3fc
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2024-02-22 11:29:39 +0000

    feat: Add route to greet a specific name with escaping

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!!!"
+    return "Hello, World!"
+
+
+@app.route("/<name>")
+def hello_name(name):
+    return f"Hello, {escape(name)}!"
 
 
 if __name__ == "__main__":
